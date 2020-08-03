90049cdb848eaff48e03ee4c2789f58fa0320558
Juan Lusnich <juanma.lusnich@gmail.com>
2020-08-03 09:14:01 -0300

    test class

---
+++ src/main/java/tests/AmazonTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import support.StepsEmbedder;
+import org.jbehave.core.embedder.Embedder;
+import org.jbehave.core.steps.Steps;
+import org.testng.annotations.Test;
+
+
+public class AmazonTests {
+
+   @Test()
+    public void alexaSearchTest() {
+        StepsEmbedder embedded= new StepsEmbedder();
+        embedded.runStory("stories/AlexaSearchTest.story");
+    }
+}
